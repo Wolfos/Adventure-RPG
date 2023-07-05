aa60708e8c6b24506bd70dc1e7d8ed16326ba69a
Robin <robin@rvanee.nl>
2023-07-05 21:55:47 +0200

    Got started on equipment and character customization

---
+++ Character/CharacterCustomization.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Character
+{
+	public class CharacterCustomization: MonoBehaviour
+	{
+		[SerializeField] private GameObject[] hair;
+	}
+}
--- Data/SaveGameManager.cs
+++ Data/SaveGameManager.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Utility;
 using WolfRPG.Core;
 
 namespace Data
